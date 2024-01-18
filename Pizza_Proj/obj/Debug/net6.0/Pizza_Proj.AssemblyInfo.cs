524ba75d187f4472d671d09dfeafa6922bffa6ec
Soumya Sutar <soumsutar9@gmail.com>
2024-01-18 10:40:00 +0530

    Soumya

--- Pizza_Proj/obj/Debug/net6.0/Pizza_Proj.AssemblyInfo.cs
+++ Pizza_Proj/obj/Debug/net6.0/Pizza_Proj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pizza_Proj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+847ba65c503e9c0e79190cd81a4e20486027e97d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pizza_Proj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pizza_Proj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
